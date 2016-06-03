23fc8ef8094809fdb7c59c6b92fccd569990a0c2
Paulo Ricardo <kyul@localhost.localdomain>
2016-06-02 23:00:38 -0300

    Added functions to calculate work and gravitational potential energy

--- fis.c
+++ fis.c
@@ -7,17 +7,19 @@
 void menu(){
 
 int choice=0;
-float speed, velocity, course_time =0;
+float speed, velocity, course_time = 0, force = 0.0f, mass = 0.0f, height = 0.0f;
 float dist=0;
 float kph, mps=0, modulo_vet;
 double angle;
 VECTOR vet;
 
-  printf("Bem Vindo a API de Fisica, Etapa 2!\n");//printa o menu bonito
+  printf("Bem Vindo a API de Fisica, Etapa 4!\n");//printa o menu bonito
   printf("-----------Escolha Operacao--------\n");
   printf("1.Decomposicao de Vetor\n");
   printf("2.Calculador de Distancia\n");
   printf("3.Conversor de K/H para M/S\n");
+  printf("4.Calcular o trabalho\n");
+  printf("5.Calcular a energia potencial gravitacional\n");
   scanf("%d", &choice);
 
   switch(choice){
@@ -49,6 +51,22 @@ VECTOR vet;
       kmp2mps(kph);
       break;
 
+    case 4:
+        printf("\nDigite a distancia percorrida pela particula: ");
+        scanf("%f", &dist);
+        printf("\nDigite a forca aplicada na particula: ");
+        scanf("%f", &force);
+        printf("O trabalho exercido pela forca e: %.4f J\n", calculate_work(force, dist, 0.0));
+        break;
+
+    case 5:
+        printf("\nDigite a altura da particula: ");
+        scanf("%f", &height);
+        printf("\nDigite a massa da particula: ");
+        scanf("%f", &mass);
+        printf("A energia potencial gravitacional da particula e: %.4f J\n", gravity_potential(height, mass));
+        break;
+
     case -1:
       break;
 
@@ -56,11 +74,7 @@ VECTOR vet;
       printf("YOUR STUPID\n");
       system("clear");
       menu();
-
   }
-
-
-
 }
 
 void init_vector(VECTOR *vect)
@@ -94,4 +108,14 @@ void kmp2mps(float kph){
 
     printf("%.3f meters per second\n", mps);
 
+}
+
+float calculate_work(float force, float displacement, float angle)
+{
+    return force * displacement * cos(angle);
+}
+
+float gravity_potential(float height, float mass)
+{
+    return height * mass * 10;
 }
