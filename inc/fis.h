7ec7732777dda7bf330fe48b91f4642906fe8872
Paulo Ricardo <kyul@localhost.localdomain>
2016-06-02 23:01:25 -0300

    Added new functions - work and gravitational potential energy

--- inc/fis.h
+++ inc/fis.h
@@ -10,4 +10,6 @@
         void decomposing_force(float magnitude_vect, double angle, VECTOR *deco_vect);
         void calcDis(float speed,float velocity,float course_time);
         void kmp2mps(float kph);
+        float calculate_work(float force, float displacement, float angle);
+        float gravity_potential(float height, float mass);
 #endif
